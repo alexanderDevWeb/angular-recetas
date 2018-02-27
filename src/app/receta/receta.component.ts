c9b35e97b9fc1560bc91312d0d47f4f99b18ae7d
alexanderDevWeb <dev.alexander.web@gmail.com>
2018-02-27 19:55:58 +0100

    config_casa_angular

--- src/app/receta/receta.component.ts
+++ src/app/receta/receta.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-receta",
-  templateUrl: "./receta.component.html",
-  styleUrls: ["./receta.component.scss"]
+  selector: 'app-receta',
+  templateUrl: './receta.component.html',
+  styleUrls: ['./receta.component.scss']
 })
 export class RecetaComponent implements OnInit {
-  //atributos
+  // atributos
   nombre: string;
   descripcion: string;
   foto: string;
@@ -17,44 +17,50 @@ export class RecetaComponent implements OnInit {
   show: boolean;
 
   constructor() {
-    console.log("RecetaComponent constructor");
-    this.nombre = "Bocata de calamares";
-    this.descripcion = "Calamares del mismísimo cantábrico";
+    console.log('RecetaComponent constructor');
+    this.nombre = 'Bocata de calamares';
+    this.descripcion = 'Calamares del mismísimo cantábrico';
     this.foto =
-      "https://cdn.pixabay.com/photo/2017/09/11/08/50/ink-2738296__340.jpg";
+      'https://cdn.pixabay.com/photo/2017/09/11/08/50/ink-2738296__340.jpg';
     this.likes = 50;
-    this.cocinero = "Karlos Argiñano";
+    this.cocinero = 'Karlos Argiñano';
     this.isGlutenFree = true;
-    this.ingredientes = ["Calamares", "Limón", "Pan", "Salsa Alioli"];
+    this.ingredientes = ['Calamares', 'Limón', 'Pan', 'Salsa Alioli'];
   }
 
   ngOnInit() {
-    console.log("RecetaComponent ngOnInit");
+    console.log('RecetaComponent ngOnInit');
   }
 
   sumarLike() {
-    console.log("Click!");
+    console.log('Click!');
     this.likes++;
   }
 
   gluten() {
-    console.log("HI");
+    console.log('HI');
     if (this.isGlutenFree) {
-      return "green";
+      return 'green';
     } else {
-      return "red";
+      return 'red';
     }
   }
 
   mostrar() {
-    console.log("Mostrarrrr");
-    // $("#listIng").css("display","block");
-    let elem = document.getElementById("listIng");
+    console.log(event);
+    // $('#listIng').css('display','block');
+    const elem = document.getElementById('listIng');
+    const caret = document.querySelector('.ingredientes i');
 
-    if (elem.style.display == "none") {
-      elem.style.display = "block";
+    console.log(elem.style.display);
+
+    if (elem.style.display === 'none'  ||  elem.style.display === ''  ) {
+      elem.style.display = 'block';
     } else {
-      elem.style.display = "none";
+      elem.style.display = 'none';
     }
+
+    caret.classList.toggle('fa-caret-up');
+    caret.classList.toggle('fa-caret-down');
   }
 }
